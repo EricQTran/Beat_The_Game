6c237ecf7bacefa8f6a33dcce7938de36a3b4c42
Eric Tran <erictran@gmail.com>
2015-08-24 12:13:18 -0700

    add GameButton

--- app/src/main/java/ericqtran/beatthegame/AddGame.java
+++ app/src/main/java/ericqtran/beatthegame/AddGame.java
@@ -1,18 +1,68 @@
 package ericqtran.beatthegame;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Stack;
 
 public class AddGame extends AppCompatActivity {
+
+    Button addGameButt;
+    TextView gameTitleInput;
+    Spinner consoleSpin;
+    Spinner percentSpin;
+    RelativeLayout touchScreen;
+    Game addedGame;
+    Stack<Game> gameStack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_game);
 
+        //initialize global variables
+        addGameButt = (Button) findViewById(R.id.addGameButton);
+        gameTitleInput = (TextView) findViewById(R.id.gameBox);
+        consoleSpin = (Spinner) findViewById(R.id.consoleSelection);
+        percentSpin = (Spinner) findViewById(R.id.percentSpinner);
+        gameStack = new Stack<>();
+        touchScreen = (RelativeLayout) findViewById(R.id.touch);
+
+        //Toast message
+        Context context = getApplicationContext();
+        CharSequence prompt = "Game added!";
+        int duration = Toast.LENGTH_SHORT;
+        final Toast message = Toast.makeText(context, prompt, duration);
+
+
+        addGameButt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String titleGame = gameTitleInput.getEditableText().toString();
+                String consoleTitle = consoleSpin.getSelectedItem().toString();
+                String percentCom = percentSpin.getSelectedItem().toString();
+                double parsedPercent = Double.parseDouble(percentCom);
+
+                addedGame = new Game(titleGame, consoleTitle,parsedPercent);
+                gameStack.push(addedGame);
+                message.show();
+
+
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/ericqtran/beatthegame/Game.java
@@ -0,0 +1,21 @@
+package ericqtran.beatthegame;
+
+/**
+ * Created by Eric on 8/23/2015.
+ */
+public class Game {
+
+    private String title;
+    private String console;
+    private double percentComplete;
+
+    public Game (String theTitle, String theConsole, double thePercentComplete)
+    {
+        title = theTitle;
+        console = theConsole;
+        percentComplete = thePercentComplete;
+    }
+
+    
+
+}
